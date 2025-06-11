0c6f3058fbce52d72ded6b89f46573fd5be0799b
Yashpratap Shah <yashpratapshah16@gmail.com>
2025-06-11 19:59:48 +0530

    new project added and some issue fixed!

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,15 +1,17 @@
 "use client"
 import { FlipWords } from "@/components/UI/flip-words";
 import { LinkPreview } from "@/components/UI/link-preview";
+import { Timeline } from "@/components/UI/timeline";
 import { Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import Image from "next/image";
 import { useEffect, useRef } from "react";
 import { BiUser } from "react-icons/bi";
-import { CgWebsite } from "react-icons/cg";
+import { CgCalendar, CgWebsite } from "react-icons/cg";
 import { CiMail } from "react-icons/ci";
-import { FaCode, FaNodeJs, FaReact, FaTerminal } from "react-icons/fa6";
+import { FaChrome, FaCode, FaNodeJs, FaReact, FaTerminal } from "react-icons/fa6";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { IoTriangle } from "react-icons/io5";
-import { SiCplusplus, SiFirebase, SiMongodb, SiNestjs, SiPostgresql, SiPython } from "react-icons/si";
+import { SiClerk, SiCplusplus, SiFirebase, SiMongodb, SiNestjs, SiPostgresql, SiPrisma, SiPython } from "react-icons/si";
 import { VscPreview } from "react-icons/vsc";
 
 
@@ -37,8 +39,54 @@ export default function Home() {
     return () => observerRef.current?.disconnect();
   }, []);
 
+  const data = [
+    {
+      title: "Project-Based Learning",
+      content: (
+        <div className="flex flex-col item-center gap-3 text-gray-400">
+          <p className="flex items-center"><CgCalendar size={20} />Date:07/2022 - present</p>
+          <p>
+            Self-driven projects aimed at personal skill enhancement
+          </p>
+          <p>
+            Developed various real-world projects to enhance full-stack development skills
+          </p>
+          <p>
+            Focused on continuous learning in emerging technologies
+          </p>
+          <p>
+            Collaborated with peers to share knowledge and foster innovation
+          </p>
+          <p>
+            Maintained documentation and analysis of project outcomes
+          </p>
+        </div>
+      )
+    },
+    {
+      title: "CODTECH",
+      content: (
+        <div className="flex flex-col item-center gap-3 text-gray-400">
+          <p className="flex items-center"><CgCalendar size={20} />Date:04/2025 - 05/2025</p>
+          <p>Remote internship for 1 month</p>
+          <p>Given Four Tasks to complete</p>
+          <p>Built various Full stack websites independently</p>
+          <p>Learned various features over the past month</p>
+          <p>All projects are on my GitHub with documentation</p>
+          <p>Certificate:</p>
+          <Image
+            alt="certificate"
+            src="/images/codtech.png"
+            height={500}
+            width={400}
+          />
+        </div>
+      )
+    }
+  ]
+
   return (
-    <div className=" min-h-screen text-green-400 font-mono">
+    <div className="relative min-h-screen text-green-400 font-mono bg-black">
 
       <div className=" h-screen flex items-center justify-center">
         <div className="relative text-center  z-10 max-w-4xl px-4">
@@ -82,8 +130,18 @@ export default function Home() {
             <h2 className="text-2xl font-bold text-gradient">About Me</h2>
           </div>
           <p className=" text-gray-400 leading-relaxed mb-12  animate-on-scroll opacity-0 translate-y-10 transition-all duration-700 delay-100">
-            Aspiring IT professional with hands-on experience in backend, frontend development. Seeking opportunities to leverage skills in Python, C++/C# And React, Next.js to contribute to innovative projects in a fast-paced IT environment
-          </p>
+            Aspiring IT professional with a focus on full-stack development and
+            project-based learning, possessing strong expertise in JavaScript and
+            Python. Key achievements include developing various real-world
+            projects independently and ranking in the top of my class with a CGPA
+            of 9.3/10. Seeking a Full Stack Developer position at your company,
+            where my full-stack development and continuous learning abilities can
+            support your mission of innovation and technological advancement.
+            Experience includes a one-month remote internship at CODTECH,
+
+            where full-stack websites were successfully built, and multiple self-
+            driven projects were developed and documented on GitHub.          </p>
+            <Timeline data={data} />
           <div className="mb-16">
             <div className="flex items-center gap-2 mx-auto mb-4  animate-on-scroll opacity-0 translate-y-10 transition-all duration-700">
               <FaCode className="w-6 h-6" />
@@ -100,6 +158,9 @@ export default function Home() {
                 { icon: SiFirebase, href: "https://firebase.google.com/", name: "Firebase" },
                 { icon: SiPython, href: "https://www.python.org/", name: "Python" },
                 { icon: SiCplusplus, href: "https://cplusplus.com/", name: "C++" },
+                { icon: SiClerk, href: "https://clerk.com/", name: "Clerk" },
+                { icon: SiPrisma, href: "https://www.prisma.io/", name: "Prisma" },
+                { icon: FaChrome, href: "https://www.convex.dev/", name: "Convex" }
               ].map((item, index) => (
                 <LinkPreview url={item.href} key={index} className=" flex items-center rounded-lg text-2xl justify-center w-full my-2 h-full  border gap-2 hover:border-green-500/60 border-green-500/30 animate-on-scroll opacity-0 translate-y-10 transition-all duration-700">
                   <item.icon className="w-8 h-8 text-green-400" />
@@ -124,6 +185,7 @@ export default function Home() {
                     url: "https://spotify-clone-flax-iota.vercel.app/",
                     code: "https://github.com/yashpratapshah16/spotify-clone",
                     src: "/images/spotify-clone.png",
+                    techstacks: ["Reactjs", "Nextjs", "Tailwind", "Typescript", "Supabase"],
                     description: "A Spotify like music app which is focus on login/sign-up and music player functionality."
                   },
                   {
@@ -131,12 +193,14 @@ export default function Home() {
                     url: "https://text-utils-theta-snowy.vercel.app/",
                     code: "https://github.com/yashpratapshah16/TextUtils",
                     src: "/images/textutils.png",
+                    techstacks: ["Reactjs", "Tailwind"],
                     description: "A basic site which have a functionality of alerting texts."
                   },
                   {
                     name: "Keeper",
                     url: "https://keeper-app-topaz.vercel.app/",
                     code: "https://github.com/yashpratapshah16/Keeper-App",
+                    techstacks: ["Reactjs", "Tailwind", "Firebase"],
                     src: "/images/keeper.png",
                     description: "An online note storing website"
                   },
@@ -145,12 +209,44 @@ export default function Home() {
                     url: "https://technowizz-7-0-round-1.vercel.app",
                     code: "https://github.com/yashpratapshah16/technowizz-7.0-round-1",
                     src: "/images/technowizz7.png",
+                    techstacks: ["Reactjs", "Tailwind", "Firebase"],
                     description: "A website which made for technowizz event of my college. A basic game site"
                   },
+                  {
+                    name: "Weather App",
+                    url: "https://weather-app-phi-five-98.vercel.app/",
+                    code: "https://github.com/yashpratapshah16/weather-app",
+                    src: "/images/weather-app.png",
+                    techstacks: ["Reactjs", "Nextjs", "Tailwind"],
+                    description: "A production-ready weather forecasting web application built with modern frontend technologies.It fetches real-time weather data from the OpenWeatherMap API and displays it in a clean, responsive UI."
+                  },
+                  {
+                    name: "QuickTalk",
+                    url: "https://quicktalk-blue.vercel.app/",
+                    code: "https://github.com/yashpratapshah16/quicktalk",
+                    src: "/images/quicktalk.png",
+                    techstacks: ["Reactjs", "Nextjs", "Tailwind", "Firebase", "Socket.io"],
+                    description: "A real-time chat application built with modern full-stack technologies for seamless one-to-one communication. Designed with performance, responsiveness, and scalability in mind, QuickTalk offers a lightweight, intuitive, and real-time messaging experience."
+                  },
+                  {
+                    name: "CoDoc",
+                    url: "https://codoc-lake.vercel.app/",
+                    code: "https://github.com/yashpratapshah16/CoDoc-Live",
+                    src: "/images/codoc.png",
+                    techstacks: ["Reactjs", "Nextjs", "Tailwind", "Convex", "Tip-Tap", "Clerk", "Liveblocks"],
+                    description: "CoDoc is a real-time collaborative document editing platform built with a modern full-stack architecture. It enables users to create, edit, and collaborate on rich-text documents simultaneously, with support for live cursors, threads, comments, and team-based permissions."
+                  },
+                  {
+                    name: "ProductiTrack",
+                    code: "https://github.com/yashpratapshah16/productitrack",
+                    src: "/images/productitrack.png",
+                    techstacks: ["React", "Vite"],
+                    description: "ProductiTrack is a lightweight and efficient Chrome Extension that monitors user activity and tracks the amount of time spent on various websites. Designed with a focus on productivity, the extension helps users understand their browsing habits and distinguish between productive and non-productive usage."
+                  },
                 ].map((item, index) => (
-                  <Card key={index} sx={{ maxWidth: 400, height: 350 }} className="flex flex-col border border-green-500 hover:border-green-400 animate-on-scroll opacity-0 translate-y-10 transition-all duration-700 ">
+                  <Card key={index} className=" h-fit flex flex-col border border-green-500 hover:border-green-400 animate-on-scroll opacity-0 translate-y-10 transition-all duration-700 ">
                     <CardMedia
-                      sx={{ height: 300 }}
+                      sx={{ height: 200 }}
                       image={item.src}
                       title={item.name}
                     />
@@ -161,6 +257,16 @@ export default function Home() {
                       <Typography variant="body2">
                         {item.description}
                       </Typography>
+                      <div className="mt-2">
+                        <h2 className="mb-2 text-lg">TechStack</h2>
+                        <div className="flex flex-wrap gap-2">
+                          {
+                            item.techstacks.map((item, index) => (
+                              <span key={index} className="h-5 w-fit border rounded-full px-2 flex items-center border-green-400">{item}</span>
+                            ))
+                          }
+                        </div>
+                      </div>
                     </CardContent>
                     <CardActions className="bg-black text-green-400 flex items-center justify-center">
                       <a target="_blank" className="group flex justify-center items-center gap-2 hover:text-white" href={item.code}>
@@ -170,13 +276,16 @@ export default function Home() {
                           <span className="absolute inset-x-0 bottom-0 h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
                         </span>
                       </a>
-                      <a target="_blank" className="group flex justify-center items-center gap-2 hover:text-white" href={item.url}>
-                        <VscPreview />
-                        <span className="relative">
-                          Preview
-                          <span className="absolute inset-x-0 bottom-0 h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
-                        </span>
-                      </a>
+                      {
+                        item.url &&
+                        <a target="_blank" className="group flex justify-center items-center gap-2 hover:text-white" href={item.url}>
+                          <VscPreview />
+                          <span className="relative">
+                            Preview
+                            <span className="absolute inset-x-0 bottom-0 h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
+                          </span>
+                        </a>
+                      }
                     </CardActions>
                   </Card>
                 ))
