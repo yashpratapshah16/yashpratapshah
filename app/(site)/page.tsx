7444817fb81dcfe85a7071e1267f7dfb8140373c
Yashpratap Shah <yashpratapshah16@gmail.com>
2025-06-26 18:41:48 +0530

    changes done!

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -290,7 +290,6 @@ export default function Home() {
                   {
                     name: "QuickTalk",
                     url: "https://quicktalk-blue.vercel.app/",
-                    code: "https://github.com/yashpratapshah16/quicktalk",
                     src: "/images/quicktalk.png",
                     techstacks: ["Reactjs", "Nextjs", "Tailwind", "Firebase", "Socket.io"],
                     description: "A real-time chat application built with modern full-stack technologies for seamless one-to-one communication. Designed with performance, responsiveness, and scalability in mind, QuickTalk offers a lightweight, intuitive, and real-time messaging experience."
